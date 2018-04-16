abdf7042cb4568887a85813d760f7366fe161194
簾内 統賀 <s01718@CG4536.local>
2018-04-16 16:29:10 +0900

    OCP

--- OCP_refact.php
+++ OCP_refact.php
@@ -2,7 +2,8 @@
 
 Const　CIRCLE_ORDER = 1;
 Const　SQUARE_ORDER = 2;
-
+$circleList = array();
+$squareList = array();
 Class Shape{
   public $drawOrder = -1;
   public function draw(){
@@ -34,7 +35,6 @@ class Square extends Shape
 };
 
 class DrawingTool{
-  //TODO 描画順で関数を分けるべきでは？仕様を要確認
   public function drawAllShapes($list)
   {
     //TODO こんな使い方できないかも。要修正
@@ -44,29 +44,36 @@ class DrawingTool{
       }
       return ($a->drawOrder < $b->drawOrder) ? -1 : 1;
     });
-
     //描画
     foreach($list => $shape){
       $shape->draw();
     }
+
+    もしくは
+
+    $circleList[]の描画
+    $squareList[]の描画
   }
 
 }
-
-public function BigCircle($radius, $point){
-  $circle = new Circle;
-  $circle->$itsRadius = $radius;
-  //@TODO $pointの値をチェックすべき
-  $itsCenter = array("x"=> $point[0], "y"=>$point[1]);
-  retrun $circle;
+class BigCircle extends Circle{
+  public function make($radius, $point){
+    $this->itsRadius = $radius;
+    //@TODO $pointの値をチェックすべき
+    $this->itsCenter = array("x"=> $point[0], "y"=>$point[1]);
+    $circleList[] = $this;
+    retrun $this;
+  }
 }
 
-public function BigSquare($side, $point){
-  $square = new Square;
-  $square->$itsSide = $side;
-  //@TODO $pointの値をチェックすべき
-  $itsTopLeft = array("x"=> $point[0], "y"=>$point[1]);
-  retrun $square;
+class BigSquare extends Square{
+  public function make($side, $point){
+    $this->itsSide = $side;
+    //@TODO $pointの値をチェックすべき
+    $this->itsTopLeft = array("x"=> $point[0], "y"=>$point[1]);
+    $squareList[] = $this;
+    retrun $this;
+  }
 }
 
 public function Point($x, $y){
@@ -75,11 +82,15 @@ public function Point($x, $y){
 }
 
 public void testDrawAllShapes() {
-  $circle = new BigCircle(10, new Point(20,20));
-  $square = new BigSquare(/*side*/ 50, /*topLeft*/ new Point(10,10));
+  $squareList = array();
+  $circleList = array();
+  $circle = new BigCircle::make(10, new Point(20,20));
+  $square = new BigSquare::make(/*side*/ 50, /*topLeft*/ new Point(10,10));
   $drawingTool = new DrawingTool;
   $shapeList = array();
   $shapeList[] = $circle;
   $shapeList[] = $square;
   $drawingTool.DrawAllShapes(shapeList);
 }
+
+//ポインタに毎回追加して、それごとに描画する？冗長になるけど変数が増えなくて便利
